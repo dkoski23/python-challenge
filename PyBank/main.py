0fee855777a13db72a22466360fba4b46faaeef6
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-15 12:26:00 -0700

    Update main.py
    ready to write to analysis file

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,7 +21,17 @@ with open(csvpath) as csvfile:
         net += int(row[1])
         if int(row[1]) > greatest_increase_num:
             greatest_increase_num = int(row[1])
-        
+            greatest_increase_date = row[0]
+        if int(row[1]) < greatest_decrease_num:
+            greatest_decrease_num = int(row[1])
+            greatest_decrease_date = row[0]
+    
+    avg_change = net/total_months
+    avg_change = round(avg_change,2)
+    print(avg_change)        
+    print(greatest_decrease_date)
+    print(greatest_decrease_num)
+    print(greatest_increase_date)   
     print(greatest_increase_num)    
     print(net)
     print(total_months)
