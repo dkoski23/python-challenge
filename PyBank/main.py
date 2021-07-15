9efd27f885eb16651e51b1af26e4eeb8b88f3361
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-15 12:14:06 -0700

    Update main.py
    some variables and iteration done

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,24 @@ import csv
 csvpath = os.path.join('.','budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
-    #print(csvreader)
+    #pull out header
+    csvheader = next(csvreader)
+    #make desired variables
+    net = 0
+    total_months = 0
+    avg_change = 0
+    greatest_increase_num = 0
+    greatest_increase_date = ''
+    greatest_decrease_num = 0
+    greatest_decrease_date = ""
+    
+    #print(csvreader) and update variables
     for row in csvreader:
-        print(row)
+        total_months +=1
+        net += int(row[1])
+        if int(row[1]) > greatest_increase_num:
+            greatest_increase_num = int(row[1])
+        
+    print(greatest_increase_num)    
+    print(net)
+    print(total_months)
