3c1da04d90a39db53829c6bb44da78a41583b4eb
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-15 11:56:29 -0700

    Update main.py
    file accessed, csvreader made, and data structure printed.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,9 @@
+import os
+import csv
 
+csvpath = os.path.join('.','budget_data.csv')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter= ',')
+    #print(csvreader)
+    for row in csvreader:
+        print(row)
