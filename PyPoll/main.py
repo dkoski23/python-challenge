a4b5ed34353369078c811a77ca70b904a46cfa94
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-16 11:46:13 -0700

    Update main.py
    Almost done, but I will try to set up a dictionary with candidates/votes and find the winner with not hard coding.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,3 @@
-import os
-import csv
-
 csvpath = os.path.join('.','election_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
@@ -29,39 +26,25 @@ with open(csvpath) as csvfile:
         if row[2] == "O'Tooley":
             otooley += 1
             
+    #make the percentage variables per candidate        
     per_khan = round(khan/total_votes, 5) * 100
-    print(per_khan)
-    per_li = round(li/total_votes, 5) * 100
-    print(per_li)
-    per_correy = round(correy/total_votes, 5) * 100
-    print(per_correy)
+    per_li = (li/total_votes) * 100
+    per_li = round(per_li, 2)
+    per_correy = round(correy/total_votes, 5) * 100   
     per_otooley = round(otooley/total_votes, 5) * 100
-    print(per_otooley)
         
         
     print(["Election Results"])
     print(["------------------------"])
     print([f"Total Votes: {total_votes}"])
     print(["------------------------"])
-    print([f"Khan: ({khan})"])
-    print([f"Li: ({li})"])
-    print([f"Correy: ({correy})"])
-    print([f"O'Tooley: ({otooley})"])
-
-    
-
-#print analysis
-#print(["Election Results"])
-#print(["------------------------"])
-#print([f"Total Votes: {total_votes}"])
-#print(["------------------------"])
-#print([f"Total: ${net}"])
-#print([f"Average Change: ${avg_change}"])
-#print([f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase_num}"])
-#print([f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease_num}"])
-        
-    
-    
+    print([f"Khan: {per_khan}% ({khan})"])
+    print([f"Li: {per_li}% ({li})"])
+    print([f"Correy: {per_correy}% ({correy})"])
+    print([f"O'Tooley: {per_otooley}% ({otooley})"])
+    print(["------------------------"])
+    print([f"Winner: ])
+    print(["------------------------"])
     
 #making variable for output file
 #output_file = os.path.join(".","pypoll_analysis.txt")
