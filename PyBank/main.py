d950281f18af3129c1cf41df17140b90dd5d9c6c
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-16 16:30:23 -0700

    Update main.py
    Worked after restarting the kernel.

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,52 +8,54 @@ with open(csvpath) as csvfile:
     #pull out header
     csvheader = next(csvreader)
     
+    
+    #make variables to use in for loop
+    monthly_profits = []
+    net = 0
+    total_months = 0    
+    #because total_months becomes 1 with the first iteration
+    #and the array of monthly changes has one less value than there are months, the indexes of
+    #the months with greatest monthly changes in profit/loss in the for loop will be 2 more than the index of the
+    #greatest monthly changes in the numpy diff_array
+    gmi_month = ""
+    gmd_month = ""
+    
+    
+    #update variables the variables made for the for loop
+    for row in csvreader:
+        total_months += 1
+        monthly_profits.append(int(row[1]))
+        net += int(row[1])
+        if total_months == 26:
+            gmi_month = row[0]
+        if total_months == 45:
+            gmd_month = row[0]
+    
     #make a numpy array to find the average difference in profit from month to month
+    #as well as the months which had the greatest change/how much of a change there was
     monthly_array = np.array(monthly_profits)
-    #print(len(monthly_array))
     diff_array = np.diff(monthly_array)
-    #print(len(diff_array))
     avg_change = np.average(diff_array)
-    avg_change = float(round(avg_diff,2))    
+    avg_change = float(round(avg_change,2))    
     greatest_increase_date = ''
     greatest_decrease_date = ""
     g_monthly_increase = max(diff_array)
-    print(g_monthly_increase)
+    #print(g_monthly_increase)
     g_monthly_decrease = min(diff_array)
-    print(g_monthly_decrease)
-    #because of total_months in the below for loop starting becoming 1 with the first value
-    #and the array of monthly changes having one less value than there are months, the indexes of
-    #the months with greatest monthly changes in profit/loss will be off by 2 from the index of the
-    #greatest monthly changes in the numpy diff_array
+    #print(g_monthly_decrease)    
     index_gmi = np.where(diff_array == g_monthly_increase)
-    print(index_gmi)
-    
-    
-    
-    
-    
-    #make desired variables
-    net = 0
-    total_months = 0
-    monthly_profits = []
-    
-    #print(csvreader) and update variables
-    for row in csvreader:
-        monthly_profits.append(int(row[1]))
-        total_months +=1
-        net += int(row[1])
-        if total_months == 26:
-            print(row[0])
-    
-    
+    #print(index_gmi)
+    index_gmd= np.where(diff_array == g_monthly_decrease)
+    #print(index_gmd)
+
 #print analysis
 print(["Financial Analysis"])
 print(["------------------------"])
 print([f"Total Months: {total_months}"])
 print([f"Total: ${net}"])
 print([f"Average Change: ${avg_change}"])
-print([f"Greatest Increase in Profits: {greatest_increase_date} $"])
-print([f"Greatest Decrease in Profits: {greatest_decrease_date} $"])
+print([f"Greatest Increase in Profits: {gmi_month} (${g_monthly_increase})"])
+print([f"Greatest Decrease in Profits: {gmd_month} (${g_monthly_decrease})"])
         
     
     
@@ -67,7 +69,7 @@ with open(output_file, "w", newline="") as analysis:
     
     writer.writerow(["Financial Analysis"])
     writer.writerow(["------------------------"])
-    writer.writerow([f"Total Months: {total_months}"])
+    writer.writerow([f"Total Months: {total_months} (${g_monthly_increase})"])
     writer.writerow([f"Total: ${net}"])
     #writer.writerow([f"Average Change: ${avg_change}"])
     #writer.writerow([f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase_num}"])
