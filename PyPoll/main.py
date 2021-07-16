8762980642ebfe5227323fb5f2775378c1bc43e9
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-16 10:20:46 -0700

    Update main.py
    Header pulled out, total votes counted, list of candidates formed, and accumulator variables for each candidate created. Next step is to increment is candidate's variable by one for each vote received, and then calculate percentages.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,20 +8,35 @@ with open(csvpath) as csvfile:
     csvheader = next(csvreader)
     #make desired variables
     total_votes = 0
-    
+    candidates = []
+    khan = 0
+    li = 0
+    otooley = 0
+    correy = 0
 
     
     #print(csvreader) and update variables
-    #for row in csvreader:
-
+    #print(csvreader)
+    for row in csvreader:
+        total_votes += 1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+    print(total_votes)
+    print(candidates)
     
 
 #print analysis
-print(["Election Results"])
-print(["------------------------"])
+#print(["Election Results"])
+#print(["------------------------"])
 #print([f"Total Votes: {total_votes}"])
-print(["------------------------"])
-  
+#print(["------------------------"])
+#print([f"Total: ${net}"])
+#print([f"Average Change: ${avg_change}"])
+#print([f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase_num}"])
+#print([f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease_num}"])
+        
+    
+    
     
 #making variable for output file
 #output_file = os.path.join(".","pypoll_analysis.txt")
