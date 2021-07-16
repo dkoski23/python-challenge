7e7fa7fcc4ba32935f5c1b6e2f6e3a347ea6ed71
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-16 15:13:48 -0700

    Update main.py
    Changing code to look for average changes from month to month, not average profits/losses. Using a numpy array to do this. Still need to find the months when the greatest changes in profitability occurred.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,8 @@
 import os
 import csv
+import numpy as np
 
-csvpath = os.path.join('.','Resources', 'budget_data.csv')
+csvpath = os.path.join('.','budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     #pull out header
@@ -9,26 +10,36 @@ with open(csvpath) as csvfile:
     #make desired variables
     net = 0
     total_months = 0
-    avg_change = 0
-    greatest_increase_num = 0
-    greatest_increase_date = ''
-    greatest_decrease_num = 0
-    greatest_decrease_date = ""
+    monthly_profits = []
     
     #print(csvreader) and update variables
     for row in csvreader:
+        monthly_profits.append(int(row[1]))
         total_months +=1
         net += int(row[1])
-        if int(row[1]) > greatest_increase_num:
-            greatest_increase_num = int(row[1])
-            greatest_increase_date = row[0]
-        if int(row[1]) < greatest_decrease_num:
-            greatest_decrease_num = int(row[1])
-            greatest_decrease_date = row[0]
+    #for item in diff_array:
+        #if item > g_monthly_increase:
+            #greatest_increase_date = row[0]
+        #if int(row[1]) < greatest_decrease_num:
+            #greatest_decrease_date = row[0]
     
-    avg_change = net/total_months
-    avg_change = round(avg_change,2)
-
+    #make a numpy array to find the average difference in profit from month to month
+    monthly_array = np.array(monthly_profits)
+    #print(monthly_array)
+    diff_array = np.diff(monthly_array)
+    #print(diff_array)
+    avg_change = np.average(diff_array)
+    avg_change = float(round(avg_diff,2))
+    
+    greatest_increase_date = ''
+    greatest_decrease_date = ""
+    g_monthly_increase = max(diff_array)
+    print(g_monthly_increase)
+    g_monthly_decrease = min(diff_array)
+    print(g_monthly_decrease)
+    test = np.where(diff_array == g_monthly_increase)
+    print(test)
+    
     
 #print analysis
 print(["Financial Analysis"])
@@ -36,14 +47,14 @@ print(["------------------------"])
 print([f"Total Months: {total_months}"])
 print([f"Total: ${net}"])
 print([f"Average Change: ${avg_change}"])
-print([f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase_num}"])
-print([f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease_num}"])
+print([f"Greatest Increase in Profits: {greatest_increase_date} $"])
+print([f"Greatest Decrease in Profits: {greatest_decrease_date} $"])
         
     
     
     
 #making variable for output file
-output_file = os.path.join(".","Analysis","pybank_analysis.txt")
+output_file = os.path.join(".","pybank_analysis.txt")
 
 #opening output file
 with open(output_file, "w", newline="") as analysis:
@@ -53,8 +64,7 @@ with open(output_file, "w", newline="") as analysis:
     writer.writerow(["------------------------"])
     writer.writerow([f"Total Months: {total_months}"])
     writer.writerow([f"Total: ${net}"])
-    writer.writerow([f"Average Change: ${avg_change}"])
-    writer.writerow([f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase_num}"])
-    writer.writerow([f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease_num}"])
-    
+    #writer.writerow([f"Average Change: ${avg_change}"])
+    #writer.writerow([f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase_num}"])
+    #writer.writerow([f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease_num}"])
     
