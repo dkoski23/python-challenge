09b212b898581fd5c8ca86249e77ff8efba15741
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-07-16 11:12:15 -0700

    Update main.py
    percentages are done, proper formatting and writing out the file are the next steps.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,7 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     #pull out header
     csvheader = next(csvreader)
-    #make desired variables
+    #make variables for total votes, list of candidates, candidate totals
     total_votes = 0
     candidates = []
     khan = 0
@@ -15,14 +15,39 @@ with open(csvpath) as csvfile:
     correy = 0
 
     
-    #print(csvreader) and update variables
-    #print(csvreader)
+    #update variables by running through the lines of the csv file
     for row in csvreader:
         total_votes += 1
         if row[2] not in candidates:
             candidates.append(row[2])
-    print(total_votes)
-    print(candidates)
+        if row[2] == "Khan":
+            khan += 1
+        if row[2] == "Li":
+            li += 1
+        if row[2] == "Correy":
+            correy += 1
+        if row[2] == "O'Tooley":
+            otooley += 1
+            
+    per_khan = round(khan/total_votes, 5) * 100
+    print(per_khan)
+    per_li = round(li/total_votes, 5) * 100
+    print(per_li)
+    per_correy = round(correy/total_votes, 5) * 100
+    print(per_correy)
+    per_otooley = round(otooley/total_votes, 5) * 100
+    print(per_otooley)
+        
+        
+    print(["Election Results"])
+    print(["------------------------"])
+    print([f"Total Votes: {total_votes}"])
+    print(["------------------------"])
+    print([f"Khan: ({khan})"])
+    print([f"Li: ({li})"])
+    print([f"Correy: ({correy})"])
+    print([f"O'Tooley: ({otooley})"])
+
     
 
 #print analysis
